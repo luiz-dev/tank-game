63b85d2ed407d18dae8213d30d2d548551625968
luiz-dev <gustavo.basic@gmail.com>
2017-03-26 16:55:09 -0300

    projeto criado na unreal

---
+++ tkGame/Source/tkGame/tkGameGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tkGame.h"
+#include "tkGameGameMode.h"
+
+
+
+
---
+++ tkGame/Source/tkGame/tkGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "tkGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TKGAME_API AtkGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
