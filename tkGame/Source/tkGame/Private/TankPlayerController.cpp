e928a6cf4b8b960ecaaf329b7df920112f49662d
luiz-dev <gustavo.basic@gmail.com>
2017-04-03 06:54:31 -0300

    Adição do c++ no jogo

---
+++ tkGame/Source/tkGame/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tkGame.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetTankControled();
+
+	if(!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing tank"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController tank possessed: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetTankControled() const
+{
+	return Cast<ATank>(GetPawn());
+}
